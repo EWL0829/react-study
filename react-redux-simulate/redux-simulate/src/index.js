a1e24c4a0a268040f39a04922a7e403fd5822596
liyue <li.yue@okcoin.com>
2018-12-11 18:43:05 +0800

    添加发布订阅部分的内容

--- react-redux-simulate/redux-simulate/src/index.js
+++ react-redux-simulate/redux-simulate/src/index.js
@@ -45,16 +45,29 @@ function stateChanger(state, action) {
 
 function createStore(state, stateChanger) {
   const getState = () => state;
-  const dispatch = (action) => stateChanger(state, action);
 
   // 发布订阅部分的内容
+  const listeners = [];
+  const subscriber = (listener) => { listeners.push(listener) };
+  const dispatch = (action) => {
+    // 第一步更新状态
+    stateChanger(state, action);
+    // 第二步执行订阅函数,如果有必要，可以加上Key去判断，哪些是需要执行的订阅函数哪些不是
+    listeners.forEach((listener) => listener());
+  };
 
-  return { getState, dispatch };
+  return { getState, dispatch, subscriber };
 }
 
 
 
 let store = createStore(appState, stateChanger);
+store.subscriber(() => renderApp(store.getState()));// 监听数据变化
+
+renderApp(store.getState());
 store.dispatch({ type: 'UPDATE_TITLE_TEXT',text: 'hello 我被修改了' });
 store.dispatch({ type: 'UPDATE_CONTENT_TEXT', content: '我是被修改后的content' });
-renderApp(store.getState());//直接将state通过store的getState方法返回
+setTimeout(() => {
+  store.dispatch({ type: 'UPDATE_TITLE_TEXT',text: 'hello 我又被修改了' });
+
+}, 3000);
